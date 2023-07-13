eca6cf9bccbf899aca6a585b60ea0679de15d26a
malmari <garrymp12@gmail.com>
2023-07-12 22:42:23 -0500

    initial commit

---
+++ src/Components/Products.jsx
@@ -0,0 +1,19 @@
+function Products() {
+  return (
+    <ul className="list-group shadow">
+      <li className="list-group-item">
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">title</h5>
+          <small className="">$abc.XY</small>
+        </div>
+        <p className="mb-1">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus,
+          sint?
+        </p>
+        <img src="" alt="gen ph img" width="200" className="" />
+      </li>
+    </ul>
+  )
+}
+
+export default Products
